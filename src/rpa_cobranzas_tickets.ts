b2ef0bcec0a99e7a53777ca8b912f49e14947740
Byron Alexander Herrera Martínez <byron.herrera@unl.edu.ec>
2025-02-21 09:38:04 -0500

    Listo Carga en Paralelo

--- src/rpa_cobranzas_tickets.ts
+++ src/rpa_cobranzas_tickets.ts
@@ -1,11 +1,13 @@
 import { login } from './core/login';
 import { interactWithElement } from './utils/handler-error';
-import { downloadFile } from './utils/handler-files';
+import pLimit from 'p-limit';
+
 
 async function runAutomation() {
 
   try {
     // 🔹 Navegar a la página e iniciar sesión
+    console.time('Tiempo de ejecución');
     console.log('🔗 Navegando a la página de inicio de sesión...');
     // Iniciar sesión en el sistema
     const { browser, page } = await login(false);
@@ -23,6 +25,7 @@ async function runAutomation() {
     // 🔹 Esperar a que cargue la lista de registro
     await interactWithElement(page, 'td:has-text("USUARIO PRUEBAS DE ACTIVACION")', 'wait', { waitTime: 2000 });
     await interactWithElement(page, 'td:has-text("USUARIO PRUEBAS DE ACTIVACION")', 'doubleClick');
+    await interactWithElement(page, 'td:has-text("USUARIO PRUEBAS DE ACTIVACION")', 'wait', { waitTime: 2000 });
 
     // 🔹 click sobre Ticket
     console.log('🎫 Creando ticket...');
@@ -33,7 +36,8 @@ async function runAutomation() {
 
     // 🔹 Esperar a que cargue el boton de editar
     await interactWithElement(page, 'button:has-text("Editar")', 'wait', { waitTime: 2000 });
-    await interactWithElement(page, 'button:has-text("Editar")', 'click');
+    await interactWithElement(page, 'button:has-text("Editar")', 'doubleClick');
+    await interactWithElement(page, 'button:has-text("Editar")', 'wait', { waitTime: 2000 });
     
     
 
@@ -41,22 +45,24 @@ async function runAutomation() {
     console.log('📝 Rellenando campo de título del ticket...');
     await interactWithElement(page, '.o_group', 'wait', { waitTime: 3000 });
 
+    await interactWithElement(page, 'input[name="name"]', 'wait', { waitTime: 2000 });
     await interactWithElement(page, 'input[name="name"]', 'fill', { text: 'ticket automatizado : Corte clientes por 5 dias' });
+    await interactWithElement(page, 'input[name="name"]', 'wait', { waitTime: 2000 });
 
     // Interactuar con el campo "Equipo"
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'wait', { waitTime: 2000 });
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'fill', { text: 'PAGOS Y COBRANZAS' });
-    
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'click');
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'wait', { waitTime: 2000 });
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'press', { key: 'ArrowDown' });
     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'press', { key: 'Enter' });
-    
+    await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'wait', { waitTime: 5000 });
+
     // Interactuar con el campo "Usuario asignado"
-    await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'wait', { waitTime: 7000 });
+    await interactWithElement(page, '.o_field_widget[name="user_id"] input.o_input',  'wait', { waitTime: 2000 });
     await interactWithElement(page, '.o_field_widget[name="user_id"] input.o_input',  'fill', { text: '	GALAN CORDOVA WILLAN ALEXANDER' });
     await interactWithElement(page, '.o_field_widget[name="user_id"] input.o_input', 'doubleClick');
-    await interactWithElement(page, '.o_field_widget[name="user_id"] input.o_input', 'press', { key: 'Tab' });
+    await interactWithElement(page, '.o_field_widget[name="user_id"] input.o_input',  'wait', { waitTime: 2000 });
 
 
     
@@ -65,23 +71,24 @@ async function runAutomation() {
     await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'fill', { text: 'PÁGINA WEB' });
     await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'press', { key: 'Enter' });
     await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'doubleClick');
-    await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'press', { key: 'Tab' });
+    await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'wait', { waitTime: 5000 });
 
      // Interactuar con el campo "Categoria"
-    await interactWithElement(page, '.o_field_widget[name="channel_id"] input.o_input', 'wait', { waitTime: 7000 });
-    await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'fill', { text: 'Pagos y cobranzas' });
     await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'wait', { waitTime: 5000 });
-
+    await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'fill', { text: 'Pagos y cobranzas' });
+    await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'wait', { waitTime: 2000 });
     await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'press', { key: 'Enter' });
+    await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'wait', { waitTime: 2000 });
 
-     // Interactuar con el campo "Etiqueta"
-     await interactWithElement(page, '.o_field_widget[name="category_id"] input.o_input', 'wait', { waitTime: 7000 });        
 
+     // Interactuar con el campo "Etiqueta"      
+     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'wait', { waitTime: 2000 });
      await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'fill', { text: 'NO PROCEDENTE' });
-     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'press', { key: 'ArrowDown' });
-     await interactWithElement(page, '.o_field_widget[name="team_id"] input.o_input', 'press', { key: 'ArrowDown' });
-     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'wait', { waitTime: 7000 });  
+     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'wait', { waitTime: 2000 });
+     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'press', { key: 'ArrowDown' });
+     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'press', { key: 'ArrowDown' });
      await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'press', { key: 'Enter' });
+     await interactWithElement(page, '.o_field_widget[name="tag_ids"] input.o_input', 'wait', { waitTime: 2000 });  
      
      // Guardar ticket
     await interactWithElement(page, 'button.btn:has-text("Guardar")', 'wait', { waitTime: 2000 });
@@ -94,8 +101,37 @@ async function runAutomation() {
     console.log('✅ ¡Proceso completado con éxito!');
   } catch (error) {
     console.error('❌ Error durante la automatización:', error);
+  } finally {
+    console.timeEnd('Tiempo de ejecución');
   }
 }
 
 // Ejecutar la automatización
 runAutomation().catch(console.error);
+
+/**
+ // Función para ejecutar múltiples robots en paralelo
+async function runRobotsInParallel(users: number, concurrency: number) {
+  const limit = pLimit(concurrency);
+
+  console.time('Tiempo total de ejecución');
+
+  const promises = Array.from({ length: users }, (_, i) =>
+    limit(async () => {
+      const delay = Math.random() * (40000 - 30000) + 30000; // Entre 30 y 40 segundos
+      await new Promise(resolve => setTimeout(resolve, delay));
+      console.log(`Iniciando robot para el usuario ${i + 1}`);
+      await runAutomation();
+      await new Promise(resolve => setTimeout(resolve, 30000));
+      console.log(`Robot para el usuario ${i + 1} completado`);
+    })
+  );
+
+  await Promise.all(promises);
+
+  console.timeEnd('Tiempo total de ejecución');
+}
+
+// Ejecutar 10 robots en paralelo
+runRobotsInParallel(10, 10).catch(console.error);
+  */
