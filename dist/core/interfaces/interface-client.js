409e0b0b0d1691a42248cd5520aa0667ff8c5a2d
leninfabricio29 <87101407+leninfabricio29@users.noreply.github.com>
2025-09-18 09:50:13 -0500

    Carga

--- dist/core/interfaces/interface-client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * @file interface-client.ts
- * @description Interfaces para la gestión de clientes y tickets
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/utils/handler-date.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.obtenerFechaModificada = void 0;
-const obtenerFechaModificada = (cantidad, unidad) => {
-    const fecha = new Date();
-    unidad === "days" ? fecha.setDate(fecha.getDate() - cantidad) : fecha.setMonth(fecha.getMonth() - cantidad);
-    return fecha.toISOString().split('T')[0]; // Formato YYYY-MM-DD
-};
-exports.obtenerFechaModificada = obtenerFechaModificada;
